b3961c8b84c0db931c3d325658c5e52dfa4291fa
Jiahao Liu <jiahaoliu5@outlook.com>
2024-05-15 11:54:25 -0400

    Test1

--- UnityMoveBox_VideoTest/Assets/Scripts/Utlis.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Utlis
-{
-    public enum AvatarType { Mixamo1, Mixamo2, RocketBox, SMPL};
-}
